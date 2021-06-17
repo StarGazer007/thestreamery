e6ae2742d4403b980707e41fe06c06848d62c823
Lisa Rodgers <lisa@spyvsspycomputers.com>
2021-06-17 02:57:15 -0400

    Create content-page.php

---
+++ template-parts/content/content-page.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * 
+ *
+ * @package WordPress
+ * @subpackage Streamer
+ * @since Streamer 1.0
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<?php if ( ! is_front_page() ) : ?>
+		<header class="entry-header alignwide">
+			<?php get_template_part( 'template-parts/header/entry-header' ); ?>
+			<?php streamer_post_thumbnail(); ?>
+		</header><!-- .entry-header -->
+	<?php elseif ( has_post_thumbnail() ) : ?>
+		<header class="entry-header alignwide">
+			<?php streamer_post_thumbnail(); ?>
+		</header><!-- .entry-header -->
+	<?php endif; ?>
+
+	<div class="entry-content">
+		<?php
+		the_content();
+
+		wp_link_pages(
+			array(
+				'before'   => '<nav class="page-links" aria-label="' . esc_attr__( 'Page', 'streamer' ) . '">',
+				'after'    => '</nav>',
+				/* translators: %: Page number. */
+				'pagelink' => esc_html__( 'Page %', 'streamer' ),
+			)
+		);
+		?>
+	</div><!-- .entry-content -->
+
+	<?php if ( get_edit_post_link() ) : ?>
+		<footer class="entry-footer default-max-width">
+			<?php
+			edit_post_link(
+				sprintf(
+					/* translators: %s: Name of current post. Only visible to screen readers. */
+					esc_html__( 'Edit %s', 'streamer' ),
+					'<span class="screen-reader-text">' . get_the_title() . '</span>'
+				),
+				'<span class="edit-link">',
+				'</span>'
+			);
+			?>
+		</footer><!-- .entry-footer -->
+	<?php endif; ?>
+</article><!-- #post-<?php the_ID(); ?> -->
