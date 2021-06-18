399004cd4bb6b0c854010655b97487af975141dd
StarGazer007 <lisa@spyvsspycomputers.com>
2021-06-18 07:03:54 -0400

    m

--- functions.php
+++ functions.php
@@ -12,34 +12,5 @@
 
 
 
-if ( ! function_exists( 'streamer_setup' ) ) {
-/**
-* Sets up theme defaults and registers support for various WordPress features
-*
-*  It is important to set up these functions before the init hook so that none of these
-*  features are lost.
-*
-*  @since MyFirstTheme 1.0
-*/
-function streamer_setup() {
 
-	/**
-	* Add post-formats support.
-	*/
-		add_theme_support(
-			'post-formats',
-			array(
-				'link',
-				'aside',
-				'gallery',
-				'image',
-				'quote',
-				'status',
-				'video',
-				'audio',
-				'chat',
-			)
-		);
-}
-
-add_action('after_setup_theme','streamer_setup');
+ ?>
