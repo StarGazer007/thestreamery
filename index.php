020fb50ddd7d07a947f71e780635f99a9dbbc440
StarGazer007 <lisa@spyvsspycomputers.com>
2021-06-18 04:50:53 -0400

    m

--- index.php
+++ index.php
@@ -16,18 +16,18 @@ if ( have_posts() ) {
 
 	// Load posts loop.
 	while ( have_posts() ) {
-		the_post();
+	//	the_post();
 
 	//	get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) );
 	}
 
 	// Previous/next page navigation.
-	streamer_the_posts_navigation();
+	//streamer_the_posts_navigation();
 
 } else {
 
 	// If no content, include the "No posts found" template.
-	get_template_part( 'template-parts/content/content-none.php' );
+//	get_template_part( 'template-parts/content/content-none.php' );
 
 }
 
