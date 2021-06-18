73c0b6f12a84f68180f78a4379f5401578a97139
StarGazer007 <lisa@spyvsspycomputers.com>
2021-06-18 04:01:24 -0400

    bug fixs

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -396,7 +396,11 @@ function streamer_password_form( $post = 0 ) {
 	$label  = 'pwbox-' . ( empty( $post->ID ) ? wp_rand() : $post->ID );
 	$output = '<p class="post-password-message">' . esc_html__( 'This content is password protected. Please enter a password to view.', 'streamer' ) . '</p>
 	<form action="' . esc_url( site_url( 'wp-login.php?action=postpass', 'login_post' ) ) . '" class="post-password-form" method="post">
-	<label class="post-password-form__label" for="' . esc_attr( $label ) . '">' . esc_html_x( 'Password', 'Post password form', 'streamer' ) . '</label><input class="post-password-form__input" name="post_password" id="' . esc_attr( $label ) . '" type="password" size="20" /><input type="submit" class="post-password-form__submit" name="' . esc_attr_x( 'Submit', 'Post password form', 'streamer' ) . '" value="' . esc_attr_x( 'Enter', 'Post password form', 'streamer' ) . '" /></form>
+	<label class="post-password-form__label" for="' . esc_attr( $label )
+	. '">' . esc_html_x( 'Password', 'Post password form', 'streamer' )
+	. '</label><input class="post-password-form__input" name="post_password" id="'
+	. esc_attr( $label ) . '" type="password" size="20" /><input type="submit" class="post-password-form__submit" name="'
+	. esc_attr_x( 'Submit', 'Post password form', 'streamer' ) . '" value="' . esc_attr_x( 'Enter', 'Post password form', 'streamer' ) . '" /></form>
 	';
 	return $output;
 }
